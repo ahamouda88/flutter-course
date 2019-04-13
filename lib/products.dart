4af5fa4fc49d9dd1e589b39e52adcdc254e3cbcb
ahamouda <ahmed.hamouda8888@gmail.com>
2019-04-13 20:38:07 +0200

    Add new font and decorate price tag

--- lib/products.dart
+++ lib/products.dart
@@ -10,7 +10,38 @@ class Products extends StatelessWidget {
       child: Column(
         children: <Widget>[
           Image.asset(products[index]['imageUrl']),
-          Text(products[index]['title']),
+          Container(
+              margin: EdgeInsets.only(top: 10.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    products[index]['title'],
+                    style: TextStyle(
+                      fontSize: 26.0,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'Oswald',
+                    ),
+                  ),
+                  SizedBox(
+                    width: 8.0,
+                  ),
+                  Container(
+                    padding: EdgeInsets.symmetric(
+                      vertical: 2.5,
+                      horizontal: 6.0,
+                    ),
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).accentColor,
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    child: Text(
+                      '\$${products[index]['price'].toString()}',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ],
+              )),
           ButtonBar(
             alignment: MainAxisAlignment.center,
             children: <Widget>[
